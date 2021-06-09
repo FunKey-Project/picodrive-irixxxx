cf8a0e0497f1ad6daa01fd13bcb04cc97375bcac
Vincent-FK <vincent.buso@funkey-project.com>
2021-06-09 23:48:06 +0200

    resynchronize audio every 10min

--- platform/common/emu.c
+++ platform/common/emu.c
@@ -1759,7 +1759,7 @@ void emu_loop(void)
 		// second changed?
 		if (timestamp_x3 - timestamp_fps_x3 >= ms_to_ticks(1000) * 3)
 		{
-#define FUNKEY_RESYNCHRONIZE_AUDIO_SECS	(5*60)
+#define FUNKEY_RESYNCHRONIZE_AUDIO_SECS	(10*60)
 #ifdef FUNKEY_RESYNCHRONIZE_AUDIO_SECS
 			static unsigned int last_resync = 0, cur_sec=0;
 			if(cur_sec++ - last_resync >= FUNKEY_RESYNCHRONIZE_AUDIO_SECS){
