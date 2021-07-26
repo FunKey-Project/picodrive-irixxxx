a653ab3bc03c6e04b301308616f89869c1f7c0d7
root <root@LAPTOP-DAVID-QA.localdomain>
2021-07-26 14:21:07 +0200

    Added submodule_update.sh

---
+++ submodule_update.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+echo ""
+echo "===== Updating Cyclone..."
+cd /opt/picodrive-irixxxx/cpu/cyclone
+git checkout master --quiet
+git pull
+echo ""
+echo "===== Updating libchdr..."
+cd /opt/picodrive-irixxxx/pico/cd/libchdr
+git checkout master --quiet
+git pull
+echo ""
+echo "===== Updating YM2413..."
+cd /opt/picodrive-irixxxx/pico/sound/emu2413
+git checkout master --quiet
+git pull
+echo ""
+echo "===== Updating libpicofe..."
+cd /opt/picodrive-irixxxx/platform/libpicofe
+git checkout FunKey --quiet
+git pull
+echo ""
+echo "===== Updating libpicofe..."
+cd /opt/picodrive-irixxxx/platform/common/dr_libs
+git checkout master --quiet
+git pull
+echo ""
+echo "===== Updating libpicofe..."
+cd /opt/picodrive-irixxxx/platform/common/minimp3
+git checkout master --quiet
+git pull
