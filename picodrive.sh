dbc96a9341d49ae1ae1ce7022dc951b58666b005
root <root@LAPTOP-DAVID-QA.localdomain>
2021-08-15 10:27:04 +0200

    Improved md5 comparison in picodrive.sh

--- picodrive.sh
+++ picodrive.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Check md5 of the PicoDrive binary in /usr/games
-myhash=$(md5sum /usr/games/PicoDrive | cut -d' ' -f1)
-if [ $myhash != "5b42024e8e3a20e5a8b58ccd203b7c90" ] || [ ! -f /usr/games/PicoDrive ]; then
+if [ `md5sum /usr/games/PicoDrive | cut -d' ' -f1` != `md5sum PicoDrive | cut -d' ' -f1` ]; then
 	rw
 	cp -f PicoDrive /usr/games
 	ro
