d7e0e766ae7121aea8eb0d4430a3748282f2d0c0
root <root@LAPTOP-DAVID-QA.localdomain>
2021-08-15 10:42:53 +0200

    Updated picodrive.sh

--- picodrive.sh
+++ picodrive.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# Check md5 of the PicoDrive binary in /usr/games
+# Compare md5 of the PicoDrive binary in /usr/games with the OPK one
 if [ `md5sum /usr/games/PicoDrive | cut -d' ' -f1` != `md5sum PicoDrive | cut -d' ' -f1` ]; then
 	rw
 	cp -f PicoDrive /usr/games
 	ro
 fi
-	./PicoDrive "$1"
+exec /usr/games/PicoDrive "$1"
