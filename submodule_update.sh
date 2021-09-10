8a585b522b65afc929cbe39add6d012a9e313cd8
root <root@LAPTOP-DAVID-QA.localdomain>
2021-09-11 00:01:16 +0200

    Changed repo name to "picodrive-funkey".

--- submodule_update.sh
+++ submodule_update.sh
@@ -1,32 +1,32 @@
 #!/bin/sh
 echo ""
 echo "===== Updating Cyclone..."
-cd /opt/picodrive-irixxxx/cpu/cyclone
+cd /opt/picodrive-funkey/cpu/cyclone
 git checkout master --quiet
 git pull
 echo ""
 echo "===== Updating libchdr..."
-cd /opt/picodrive-irixxxx/pico/cd/libchdr
+cd /opt/picodrive-funkey/pico/cd/libchdr
 git checkout master --quiet
 git pull
 echo ""
 echo "===== Updating YM2413..."
-cd /opt/picodrive-irixxxx/pico/sound/emu2413
+cd /opt/picodrive-funkey/pico/sound/emu2413
 git checkout master --quiet
 git pull
 echo ""
 echo "===== Updating libpicofe..."
-cd /opt/picodrive-irixxxx/platform/libpicofe
+cd /opt/picodrive-funkey/platform/libpicofe
 git checkout FunKey --quiet
 git pull
 echo ""
 echo "===== Updating dr_libs..."
-cd /opt/picodrive-irixxxx/platform/common/dr_libs
+cd /opt/picodrive-funkey/platform/common/dr_libs
 git checkout master --quiet
 git pull
 echo ""
 echo "===== Updating minimp3..."
-cd /opt/picodrive-irixxxx/platform/common/minimp3
+cd /opt/picodrive-funkey/platform/common/minimp3
 git checkout master --quiet
 git pull
 echo ""
