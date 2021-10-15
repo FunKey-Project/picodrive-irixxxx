1129da87fc936fc95a1176e0f6662724c926e4ed
root <cruau.david@gmail.com>
2021-10-15 09:49:31 +0200

    Added .gg file extension support for GMenu2X and RetroFE.

--- picodrive.sh
+++ picodrive.sh
@@ -3,6 +3,7 @@
 if [ `md5sum /usr/games/PicoDrive | cut -d' ' -f1` != `md5sum PicoDrive | cut -d' ' -f1` ]; then
 	rw
 	cp -f PicoDrive /usr/games
+	sed -i 's|list.extensions.*|list.extensions = zip,ZIP,sms,SMS,bin,BIN,gg,GG|' /usr/games/collections/Sega\ Master\ System/settings.conf
 	ro
 fi
 exec /usr/games/launchers/megadrive_launch.sh "$1"
